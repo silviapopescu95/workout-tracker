998560b5450b6b9ffd44fc76143f0d81f55a5ace
Silvia Popescu <amazingaivlis@gmail.com>
2021-01-03 13:35:02 -0800

    Added route to index.html.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,12 +5,16 @@ var router = require("express").Router();
 var path = require("path");
 
 // Routes
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/assets/html/index.html"));
+});
+
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/assets/html/exercise.html"));
-  });
-  
-  router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/assets/html/stats.html"));
-  });
-  
-  module.exports = router;
+  res.sendFile(path.join(__dirname, "../public/assets/html/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/assets/html/stats.html"));
+});
+
+module.exports = router;
